a54568adf7b5a680d6890205954e69aef82d5e93
aaquinox <145797278+aaquinox@users.noreply.github.com>
2023-11-14 13:47:27 -0500

    updated code and added animation.

--- Platformer Game/Assets/Scripts/PlayerController.cs
+++ Platformer Game/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    //Animation Variables
+    Animater anim;
+    public bool moving = false;
 
+    //Movement Variables
     Rigidbody2D rb; //create reference fro rigidbody bc jump physics
     public float jumpForce; //the force that will be added to the vertical component of player's velocity
     public float speed;
@@ -23,6 +27,7 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -38,19 +43,22 @@ public class PlayerController : MonoBehaviour
         {
             newPosition.x -= speed;
             newScale.x = -currentScale;
+            moving = true;
         }
 
         if(Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow))
         {
             newPosition.x += speed;
             newScale.x = currentScale;
+            moving = true;
         }
 
         if((Input.GetKeyDown("w") || Input.GetKey(KeyCode.UpArrow)) && isGrounded == true)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
-
+        
+        
         transform.position = newPosition;
         transform.localScale = newScale;
     }
